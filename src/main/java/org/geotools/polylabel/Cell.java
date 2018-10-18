4fc4cc1b9b87cdba324186cad40138a6c5ec213e
Martin Davis <mtnclimb@gmail.com>
2018-10-18 10:26:34 -0700

    Port distance improvements from Geotools master

--- src/main/java/org/geotools/polylabel/Cell.java
+++ src/main/java/org/geotools/polylabel/Cell.java
@@ -1,9 +1,9 @@
 /*
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
- * 
+ *
  *    (C) 2016, Open Source Geospatial Foundation (OSGeo)
- *    
+ *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation;
@@ -13,7 +13,7 @@
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
- *    
+ *
  */
 package org.geotools.polylabel;
 
@@ -21,19 +21,14 @@ import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.MultiPolygon;
 import org.locationtech.jts.geom.Point;
-import org.locationtech.jts.operation.distance.DistanceOp;
-
-
-
 
 /**
  * Based on Vladimir Agafonkin's Algorithm https://www.mapbox.com/blog/polygon-center/
- * 
- * Implementation of quadtree cells for "Pole of inaccessibility". 
- * 
+ *
+ * <p>Implementation of quadtree cells for "Pole of inaccessibility".
+ *
  * @author Ian Turton
  * @author Casper Børgesen
- * 
  */
 public class Cell implements Comparable<Cell> {
 
@@ -51,18 +46,18 @@ public class Cell implements Comparable<Cell> {
 
     private Point p;
 
-    Cell(double x, double y, double h, Geometry polygon) {
+    Cell(double x, double y, double h, Geometry multiPolygon) {
 
         this.setX(x); // cell center x
         this.setY(y); // cell center y
         this.setH(h); // half the cell size
-        p = polygon.getFactory().createPoint(new Coordinate(x,y));
+        p = multiPolygon.getFactory().createPoint(new Coordinate(x, y));
 
         // distance from cell center to polygon
-        this.setD(pointToPolygonDist(p, polygon)); 
-        
+        this.setD(pointToPolygonDist(p, multiPolygon));
+
         // max distance to polygon within a cell
-        this.setMax(this.getD() + this.getH() * SQRT2); 
+        this.setMax(this.getD() + this.getH() * SQRT2);
     }
 
     @Override
@@ -77,12 +72,51 @@ public class Cell implements Comparable<Cell> {
 
     // signed distance from point to polygon outline (negative if point is
     // outside)
-    private double pointToPolygonDist(Point point, Geometry polygon) {
-        boolean inside = polygon.contains(point);
-        double dist = DistanceOp.distance(point, polygon.getBoundary());
+    private double pointToPolygonDist(Point point, Geometry multiPolygon) {
+
+        boolean inside = false;
+        double minDistSq = Double.POSITIVE_INFINITY;
+
+        for (int k = 0; k < multiPolygon.getNumGeometries(); k++) {
+            Coordinate[] ring = multiPolygon.getGeometryN(k).getCoordinates();
+            for (int i = 0, len = ring.length, j = len - 1; i < len; j = i++) {
+                Coordinate a = ring[i];
+                Coordinate b = ring[j];
+                if ((a.y > y != b.y > y) && (x < (b.x - a.x) * (y - a.y) / (b.y - a.y) + a.x))
+                    inside = !inside;
+                minDistSq = Math.min(minDistSq, getSegDistSq(x, y, a, b));
+            }
+        }
 
         // Points outside has a negative distance and thus will be weighted down later.
-        return (inside ? 1 : -1) * dist;
+        return (inside ? 1 : -1) * Math.sqrt(minDistSq);
+    }
+    // get squared distance from a point to a segment
+    private double getSegDistSq(double px, double py, Coordinate a, Coordinate b) {
+
+        double x = a.x;
+        double y = a.y;
+        double dx = b.x - x;
+        double dy = b.y - y;
+
+        if (dx != 0.0 || dy != 0.0) {
+
+            double t = ((px - x) * dx + (py - y) * dy) / (dx * dx + dy * dy);
+
+            if (t > 1) {
+                x = b.x;
+                y = b.y;
+
+            } else if (t > 0) {
+                x += dx * t;
+                y += dy * t;
+            }
+        }
+
+        dx = px - x;
+        dy = py - y;
+
+        return dx * dx + dy * dy;
     }
 
     public double getMax() {
